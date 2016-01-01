752cabd6dee2d81218dbac26db78a8fb85b6cd71
Álex Sáez <alex.saez.12@gmail.com>
2016-01-01 19:35:41 +0100

    Some style modifications

--- src/pyfme/models/euler_6DOF_flat_earth.py
+++ src/pyfme/models/euler_6DOF_flat_earth.py
@@ -79,13 +79,13 @@ def linear_and_angular_momentum_eqs(time, vel, mass, inertia, forces, moments):
     dw_dt = Fz / mass + q * u - p * v
 
     # Angular momentum equations
-    dp_dt = (L * Iz + N * Jxz - q * r * (Iz ** 2 - Iz * Iy + Jxz ** 2) + p * q\
-            * Jxz * (Ix + Iz - Iy)) / (Ix * Iz - Jxz ** 2)
+    dp_dt = (L * Iz + N * Jxz - q * r * (Iz ** 2 - Iz * Iy + Jxz ** 2) +
+             p * q * Jxz * (Ix + Iz - Iy)) / (Ix * Iz - Jxz ** 2)
 
-    dq_dt = (M  + (Iz - Ix) * p * r - Jxz * (p ** 2 - r ** 2)) / Iy
+    dq_dt = (M + (Iz - Ix) * p * r - Jxz * (p ** 2 - r ** 2)) / Iy
 
-    dr_dt = (L * Jxz + N * Ix + p * q * (Ix ** 2 - Ix * Iy + Jxz ** 2) - q * r\
-            * Jxz * (Iz + Ix - Iy)) / (Ix * Iz - Jxz ** 2)
+    dr_dt = (L * Jxz + N * Ix + p * q * (Ix ** 2 - Ix * Iy + Jxz ** 2) -
+             q * r * Jxz * (Iz + Ix - Iy)) / (Ix * Iz - Jxz ** 2)
 
 
     return np.array([du_dt, dv_dt, dw_dt, dp_dt, dq_dt, dr_dt])
@@ -104,33 +104,33 @@ def jac_linear_and_angular_momentum_eqs(time, vel, mass, inertia):
 
     jac = np.zeros([6,6])
 
-    jac[0,1] = r
-    jac[0,2] = - q
-    jac[0,4] = - w
-    jac[0,5] = v
+    jac[0, 1] = r
+    jac[0, 2] = - q
+    jac[0, 4] = - w
+    jac[0, 5] = v
 
-    jac[1,0] = - r
-    jac[1,2] = p
-    jac[1,3] = w
-    jac[1,5] = - u
+    jac[1, 0] = - r
+    jac[1, 2] = p
+    jac[1, 3] = w
+    jac[1, 5] = - u
 
-    jac[2,0] = q
-    jac[2,1] = - p
-    jac[2,3] = - v
-    jac[2,4] = u
+    jac[2, 0] = q
+    jac[2, 1] = - p
+    jac[2, 3] = - v
+    jac[2, 4] = u
 
-    jac[3,3] = Jxz * q * (Ix + Iz - Iy) / (Ix * Iz - Jxz ** 2)
-    jac[3,4] = (p * Jxz * (Ix + Iz - Iy) - r * (Iz ** 2 - Iz * Iy + Jxz ** 2))\
+    jac[3, 3] = Jxz * q * (Ix + Iz - Iy) / (Ix * Iz - Jxz ** 2)
+    jac[3, 4] = (p * Jxz * (Ix + Iz - Iy) - r * (Iz ** 2 - Iz * Iy + Jxz ** 2))\
+                / (Ix * Iz - Jxz ** 2)
+    jac[3, 5] = - q * (Iz ** 2 - Iz * Iy + Jxz ** 2) / (Ix * Iz - Jxz ** 2)
+
+    jac[4, 3] = ((Iz - Ix) * r - 2 * Jxz * p) / Iy
+    jac[4, 5] = ((Iz - Ix) * p + 2 * Jxz * r) / Iy
+
+    jac[5, 3] = q * (Ix ** 2 - Ix * Iy + Jxz ** 2) / (Ix * Iz - Jxz ** 2)
+    jac[5, 4] = (p * (Ix ** 2 - Ix * Iy + Jxz ** 2) - r * Jxz * (Ix + Iz - Iy))\
                / (Ix * Iz - Jxz ** 2)
-    jac[3,5] = - q * (Iz ** 2 - Iz * Iy + Jxz ** 2) / (Ix * Iz - Jxz ** 2)
-
-    jac[4,3] = ((Iz - Ix) * r - 2 * Jxz * p) / Iy
-    jac[4,5] = ((Iz - Ix) * p + 2 * Jxz * r) / Iy
-
-    jac[5,3] = q * (Ix ** 2 - Ix * Iy + Jxz ** 2) / (Ix * Iz - Jxz ** 2)
-    jac[5,4] = (p * (Ix ** 2 - Ix * Iy + Jxz ** 2) - r * Jxz * (Ix + Iz - Iy))\
-               / (Ix * Iz - Jxz ** 2)
-    jac[5,5] = - q * Jxz * (Iz + Ix - Iy) / (Ix * Iz - Jxz ** 2)
+    jac[5, 5] = - q * Jxz * (Iz + Ix - Iy) / (Ix * Iz - Jxz ** 2)
 
     return jac
 
@@ -161,14 +161,14 @@ def jac_kinematic_angular_eqs(time, euler_angles, angular_vel):
 
     jac = np.zeros([3,3])
 
-    jac[0,1] = - (q * np.sin(phi) + r * np.cos(phi))
+    jac[0, 1] = - (q * np.sin(phi) + r * np.cos(phi))
 
-    jac[1,0] = (q * np.sin(phi) + r * np.cos(phi)) / np.cos(theta) ** 2
-    jac[1,1] = (q * np.cos(phi) - r * np.sin(phi)) * np.tan(theta)
+    jac[1, 0] = (q * np.sin(phi) + r * np.cos(phi)) / np.cos(theta) ** 2
+    jac[1, 1] = (q * np.cos(phi) - r * np.sin(phi)) * np.tan(theta)
 
-    jac[2,0] = - (q * np.sin(phi) + r * np.cos(phi)) * (np.tan(theta) / \
-               np.cos(theta))
-    jac[2,1] = (q * np.cos(phi) - r * np.sin(phi)) / np.cos(theta)
+    jac[2, 0] = - (q * np.sin(phi) + r * np.cos(phi)) * (np.tan(theta) /
+                   np.cos(theta))
+    jac[2, 1] = (q * np.cos(phi) - r * np.sin(phi)) / np.cos(theta)
 
     return jac
 
@@ -181,12 +181,12 @@ def navigation_eqs(time, pos, vel, euler_angles):
     u, v, w = vel
     theta, phi, psi = euler_angles
 
-    dx_dt = np.cos(theta) * np.cos(phi) * u + (np.sin(phi) * np.sin(theta) *\
-            np.cos(psi) - np.cos(phi) * np.sin(psi)) * v + (np.cos(phi) * \
+    dx_dt = np.cos(theta) * np.cos(phi) * u + (np.sin(phi) * np.sin(theta) *
+            np.cos(psi) - np.cos(phi) * np.sin(psi)) * v + (np.cos(phi) *
             np.sin(theta) * np.cos(psi) + np.sin(phi) * np.sin(psi)) * w
 
-    dy_dt = np.cos(theta) * np.sin(psi) * u + (np.sin(phi) * np.sin(theta) * \
-            np.sin(psi) +  np.cos(phi) * np.cos(psi)) * v + (np.cos(phi) * \
+    dy_dt = np.cos(theta) * np.sin(psi) * u + (np.sin(phi) * np.sin(theta) *
+            np.sin(psi) +  np.cos(phi) * np.cos(psi)) * v + (np.cos(phi) *
             np.sin(theta) * np.sin(psi) - np.sin(phi) * np.cos(psi)) * w
 
     dz_dt = - np.sin(theta) * u + np.sin(phi) * np.cos(theta) * v + \
