bf1ac6ffdee308e5f704622936534935dfbeb7ec
Your Name <juan.matesanz.saiz@gmail.com>
2016-01-05 22:22:21 +0100

    import sin and cos from numpy, add test for wind-hor

--- src/pyfme/utils/tests/test_coordinates.py
+++ src/pyfme/utils/tests/test_coordinates.py
@@ -10,7 +10,11 @@ from numpy.testing import (assert_equal, assert_almost_equal,
 
 
 from pyfme.utils.coordinates import (body2hor, hor2body,
-                                     check_theta_phi_psi_range)
+                                     check_theta_phi_psi_range,
+                                     hor2wind, wind2hor,
+                                     check_gamma_mu_chi_range,
+                                     body2wind, wind2body,
+                                     check_alpha_beta_range)
 
 
 def test_check_theta_phi_psi_range():
@@ -90,3 +94,94 @@ def test_hor2body():
     vector_body = hor2body(vector_hor, theta, phi, psi)
 
     assert_array_almost_equal(vector_body, vector_body_expected)
+
+
+def test_check_gamma_mu_chi_range():
+
+    wrong_values = (3 * np.pi, - 3 * np.pi)
+
+    for value in wrong_values:
+        # 0 is always a correct value
+        angles = [0, 0, 0]
+        for ii in range(3):
+            angles[ii] = value
+            with pytest.raises(ValueError):
+                check_theta_phi_psi_range(*angles)
+
+                
+def test_wind2hor():
+
+    # Test with a pitch rotation
+    vector_wind = np.array([1, 1, 1])
+    gamma, mu, chi = np.deg2rad(45), 0, 0
+
+    vector_hor = wind2hor(vector_wind, gamma, mu, chi)
+
+    vector_hor_expected = np.array([2 * 0.70710678118654757, 1, 0])
+
+    assert_array_almost_equal(vector_hor, vector_hor_expected)
+
+    # Test with a roll rotation
+    vector_wind = np.array([1, 1, 1])
+    gamma, mu, chi = 0, np.deg2rad(45), 0
+
+    vector_hor = wind2hor(vector_wind, gamma, mu, chi)
+
+    vector_hor_expected = np.array([1, 0, 2 * 0.70710678118654757])
+
+    assert_array_almost_equal(vector_hor, vector_hor_expected)
+
+    # Test with a yaw rotation
+    vector_hor = np.array([1, 1, 1])
+    gamma, mu, chi = 0, 0, np.deg2rad(45)
+
+    vector_hor = wind2hor(vector_wind, gamma, mu, chi)
+
+    vector_hor_expected = np.array([0, 2 * 0.70710678118654757, 1])
+
+    assert_array_almost_equal(vector_hor, vector_hor_expected)
+
+
+def test_hor2wind():
+
+    # Test with a pitch rotation
+    vector_hor = np.array([2 * 0.70710678118654757,  1, 0])
+    gamma, mu, chi = np.deg2rad(45), 0, 0
+
+    vector_wind_expected = np.array([1, 1, 1])
+
+    vector_wind = hor2wind(vector_hor, gamma, mu, chi)
+
+    assert_array_almost_equal(vector_wind, vector_wind_expected)
+
+    # Test with a roll rotation
+    vector_hor = np.array([1, 0, 2 * 0.70710678118654757])
+    gamma, mu, chi = 0, np.deg2rad(45), 0
+
+    vector_wind_expected = np.array([1, 1, 1])
+
+    vector_wind = hor2wind(vector_wind, gamma, mu, chi)
+
+    assert_array_almost_equal(vector_wind, vector_wind_expected)
+
+    # Test with a yaw rotation
+    vector_hor = np.array([0, 2 * 0.70710678118654757, 1])
+    gamma, mu, chi = 0, 0, np.deg2rad(45)
+
+    vector_wind_expected = np.array([1, 1, 1])
+
+    vector_wind = hor2wind(vector_hor, gamma, mu, chi)
+
+    assert_array_almost_equal(vector_wind, vector_wind_expected)
+
+def test_check_alpha_beta_range():
+
+    wrong_values = (3 * np.pi, - 3 * np.pi)
+
+    for value in wrong_values:
+        # 0 is always a correct value
+        angles = [0, 0]
+        for ii in range(2):
+            angles[ii] = value
+            with pytest.raises(ValueError):
+                check_theta_phi_psi_range(*angles)
