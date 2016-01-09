4aed38e4a33128656145b0a06bba906ba133028f
JuanMatSa <juan.matesanz.saiz@gmail.com>
2016-01-09 13:42:01 +0100

    falta lanzarlo y los test

--- src/pyfme/ball/ball.py
+++ src/pyfme/ball/ball.py
@@ -6,9 +6,11 @@ Created on Fri Jan  8 18:53:17 2016
 """
 
 import numpy as np
+from pyfme.utils.coordinates import wind2body
+from pyme.environment.isa import atm
 
 
-def Geometric_Data():
+def Geometric_Data(r=0.111):
 
     """ Provides the value of some geometric data.
 
@@ -18,6 +20,7 @@ def Geometric_Data():
     r    radius(m)
     S_circle    Surface (m^2)
     S_sphere    Surface (m^2)
+    Vol    Volume (m^3)
 
     References
     ----------
@@ -36,14 +39,15 @@ def Geometric_Data():
     ----------
     .. [1]
     """
-    r = 0.111
+
     S_circle = np.pi * r ** 2
     S_sphere = 4 * np.pi * r ** 2
+    Vol = 4 / 3 * np.pi * r ** 3
 
-    return r, S_circle, S_sphere
+    return r, S_circle, S_sphere, Vol
 
 
-def Mass_and_Inertial_Data(r):
+def Mass_and_Inertial_Data(r, mass=0.440):
 
     """ Provides the value of some mass and inertial data.
 
@@ -70,7 +74,6 @@ def Mass_and_Inertial_Data(r):
     .. [1]
     """
 
-    mass = 0.440
     Ixxb = 2 * mass * (r ** 2) / 3
     Iyyb = Ixxb
     Izzb = Ixxb
@@ -80,31 +83,38 @@ def Mass_and_Inertial_Data(r):
     return mass, I_matrix
 
 
-def Ball_forces(velocity_vector, rho, radius):
-    """ Given a vector velocity gives the forces and moments.
+def Ball_forces(velocity_vector, rho, alpha, beta):
+    """ Given a velocity vector (body axes) provides the forces (body axis).
     Data for a soccer ball (smooth sphere)
 
     Data
     -----
-    velocity_vector = np.array([u, v, w, p, q, r])    air velocity body-axes
-                                                        (m/s)
-    rho    air density(m/kg^3)
     A_front    Front section (m^2)
-    C_magnus    magnus effect coefficient force
-    F_magnus    magnus force (Pa)
     Cd    Drag coefficient
+    C_magnus    magnus effect coefficient force
+
     D    drag force (Pa)
-    Sn    effective spin number (wn*r/V)
-    V    velocity modulus (m/s)
-    wn    angular velocity perpendicular to the linear velocity (m/s)
-    Re    Reynolds number
+    Dx_body    drag force x-body-axes(Pa)
+    Dy_body    drag force y-body-axes(Pa)
+    Dz_body    drag force z-body-axes(Pa)
+    D_vector_body    Drag forces vector (body axes) (Pa)
+
+    dir_F_magnus    director vector of the magnus Force (body axes)
+    F_magnus    magnus force (Pa)
+    F_magnus_vector_body    magnus Forces vector (body axes) (Pa)
+    Fx_magnus_body    magnus Forces x-body-axes (Pa)
+    Fy_magnus_body    magnus Forces y-body-axes (Pa)
+    Fz_magnus_body    magnus Forces z-body-axes (Pa)
+
     mu = 1.983 e-5   viscosity (kg/s/m)
     radius    (m)
-    dir_F_magnus    director vector of the magnus Force (body axes)
-    F_magnus_vector_body    magnus Forces vector (body axes)
-    Fx_magnus_body    magnus Forces x-body-axes
-    Fy_magnus_body    magnus Forces y-body-axes
-    Fz_magnus_body    magnus Forces z-body-axes
+    rho    air density(m/kg^3)
+    Re    Reynolds number
+    Sn    effective spin number (wn*r/V)
+    V    velocity modulus (m/s)
+    velocity_vector = np.array([u, v, w, p, q, r])    air velocity body-axes
+                                                                          (m/s)
+    wn    angular velocity perpendicular to the linear velocity (m/s)
 
     Sn    C_magnus    [1]
     ---------------
@@ -147,8 +157,6 @@ def Ball_forces(velocity_vector, rho, radius):
     [1]
     """
 
-    A_front = np.pi * radius ** 2
-
     mu = 1.983 * 10 ** -5
     Re_list = [38000, 100000, 160000, 200000, 250000, 300000, 330000, 350000,
                375000, 400000, 500000, 800000, 200000, 4000000]
@@ -159,8 +167,10 @@ def Ball_forces(velocity_vector, rho, radius):
 
     u, v, w = velocity_vector[:3]
     p, q, r = velocity_vector[3:]
-
     V = np.linalg.norm(velocity_vector[:3])
+
+    radius, A_front, _, _ = Geometric_Data()
+    rho = atm(0)[2]
     Re = rho * V * radius / mu
 
     if Re < Re_list[0]:
@@ -186,10 +196,17 @@ def Ball_forces(velocity_vector, rho, radius):
 
     F_magnus = 0.5 * rho * V ** 2 * A_front * C_magnus
     dir_F_magnus = np.array([v * r - w * q, w * p - u * r, u * q - v * p]) / wn
+
     F_magnus_vector_body = dir_F_magnus * F_magnus
     Fx_magnus_body = F_magnus_vector_body[0]
     Fy_magnus_body = F_magnus_vector_body[1]
     Fz_magnus_body = F_magnus_vector_body[2]
 
+    D_vector_body = wind2body(([D, 0, 0]), alpha, beta)
+    Dx_body = D_vector_body[0]
+    Dy_body = D_vector_body[1]
+    Dz_body = D_vector_body[2]
+
     return D, F_magnus, Cd, C_magnus, F_magnus_vector_body, Fx_magnus_body, \
-        Fy_magnus_body, Fz_magnus_body
+        Fy_magnus_body, Fz_magnus_body, D_vector_body, Dx_body, Dy_body,\
+        Dz_body
