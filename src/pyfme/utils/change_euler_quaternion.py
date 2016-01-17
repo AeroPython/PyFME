354e9f0da0592ae0fddc8b03e58d25a55f690d7b
JuanMatSa <juan.matesanz.saiz@gmail.com>
2016-01-17 13:48:22 +0100

    derivative quaternion added, test added and passed.

--- src/pyfme/utils/change_euler_quaternion.py
+++ src/pyfme/utils/change_euler_quaternion.py
@@ -83,6 +83,47 @@ def euler2quatern(euler_angles):
     return quaternion
 
 
+def vel_quaternion(quaternion, ang_vel):
+    '''Given the angular velocity vector and the quaternion,
+    the derivative of the quaternion vector is returned.
+
+    Parameters
+    ----------
+    quaternion : array_like
+        1x4 vector with the four elements of the quaternion: q_0, q_1, q_2, q_3
+
+    ang_vel : array_like
+        (p, q, r) Absolute angular velocity of aircraft with respect to
+        inertial frame of ref in body axes coordinates
+
+    Returns
+    -------
+    d_quaternion : array_like
+        1x4 vector with the derivative of the four elements of the quaternion:
+        [d_q_0, d_q_1, d_q_2, d_q_3]
+
+    References
+    ----------
+    .. [1] "Modeling and Simulation of Aerospace Vehicle Dynamics" (Aiaa\
+        Education Series) Peter H. Ziepfel
+    '''
+    p, q, r = ang_vel
+
+    q_0, q_1, q_2, q_3 = quaternion
+
+    d_q_0 = 0.5 * (-p * q_1 - q * q_2 - r * q_3)
+
+    d_q_1 = 0.5 * (p * q_0 + r * q_2 - q * q_3)
+
+    d_q_2 = 0.5 * (q * q_0 - r * q_1 + p * q_3)
+
+    d_q_3 = 0.5 * (r * q_0 + q * q_1 - p * q_2)
+
+    d_quaternion = np.array([d_q_0, d_q_1, d_q_2, d_q_3])
+
+    return d_quaternion
+
+
 def check_unitnorm(quaternion):
     '''Given a quaternion, it checks the modulus (it must be unit). If it is
     not unit, it raises an error.
@@ -92,7 +133,6 @@ def check_unitnorm(quaternion):
     quaternion : array_like
         1x4 vector with the four elements of the quaternion:
         [q_0, q_1, q_2, q_3]
-
     Raises
     ------
     ValueError:
--- src/pyfme/utils/tests/test_euler_quaternion.py
+++ src/pyfme/utils/tests/test_euler_quaternion.py
@@ -10,21 +10,71 @@ import numpy as np
 from numpy.testing import (assert_array_almost_equal)
 
 
-from pyfme.utils.coordinates import (quatern2euler, euler2quatern,
-                                     check_unitnorm)
+from pyfme.utils.change_euler_quaternion import (quatern2euler, euler2quatern,
+                                                 check_unitnorm,
+                                                 vel_quaternion)
 
 
 def test_quatern2euler():
 
     quaternion = np.array([0.8660254037844387, 0, 0.5, 0])
 
-    euler_angles_expected = np.array([0.0, 1.0471975511965976, 0.0])
+    euler_angles_expected = np.array([1.04719755, 0.0, 0.0])
 
     euler_angles = quatern2euler(quaternion)
 
     assert_array_almost_equal(euler_angles, euler_angles_expected)
 
 
-print(quatern2euler(0.8660254037844387, 0, 0.5, 0))
+def test_euler2quatern():
 
-print(euler2quatern(0.0, 1.0471975511965976, 0.0))
+    euler_angles = np.array([0.0, 1.0471975511965976, 0.0])
+
+    quaternion_expected = np.array([0.8660254037844387, 0.5, 0, 0])
+
+    quaternion = euler2quatern(euler_angles)
+
+    assert_array_almost_equal(quaternion, quaternion_expected)
+
+
+def test_vel_quaternion():
+    # test for bank velocity p = 1
+    quaternion = np.array([0.8660254037844387, 0, 0.5, 0])
+
+    ang_vel = np.array([1, 0, 0])
+
+    d_quaternion_expected = np.array([0, 0.4330127018922193, 0, -0.25])
+
+    d_quaternion = vel_quaternion(quaternion, ang_vel)
+
+    assert_array_almost_equal(d_quaternion, d_quaternion_expected)
+
+    # test for pitch velocity q = 1
+    quaternion = np.array([0.8660254037844387, 0, 0.5, 0])
+
+    ang_vel = np.array([0, 1, 0])
+
+    d_quaternion_expected = np.array([-0.25, 0, 0.4330127018922193, 0])
+
+    d_quaternion = vel_quaternion(quaternion, ang_vel)
+
+    assert_array_almost_equal(d_quaternion, d_quaternion_expected)
+
+    # test for yaw velocity r = 1
+    quaternion = np.array([0.8660254037844387, 0, 0.5, 0])
+
+    ang_vel = np.array([0, 0, 1])
+
+    d_quaternion_expected = np.array([0, 0.25, 0, 0.4330127018922193])
+
+    d_quaternion = vel_quaternion(quaternion, ang_vel)
+
+    assert_array_almost_equal(d_quaternion, d_quaternion_expected)
+
+
+def test_check_unitnorm():
+
+    wrong_value = ([1, 0, 0.25, 0.5])
+
+    with pytest.raises(ValueError):
+        check_unitnorm(wrong_value)
