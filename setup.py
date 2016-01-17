fd2b32cf407c640c1639000c99264a195d7cd2f8
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2016-01-17 15:57:26 +0100

    Fix setup.py
    Every Python project in the world has a "Fix setup.py" commit.

--- setup.py
+++ setup.py
@@ -6,6 +6,14 @@ from distutils.core import setup
 setup(
     name="PyFME",
     version="0.1.dev0",
-    packages=['pyfme'],
+    packages=[
+        'pyfme',
+        'pyfme.aero',
+        'pyfme.aircrafts',
+        'pyfme.environment',
+        'pyfme.models',
+        'pyfme.prop',
+        'pyfme.utils'
+    ],
     package_dir={'': 'src'},
 )
