0d8e539f8513bdacd08a203b11a12a180d657f9a
DLpadilla <dani_yatero3@hotmail.com>
2016-02-14 18:20:04 +0100

    test_altimetry modified

--- src/pyfme/utils/tests/test_altimetry.py
+++ src/pyfme/utils/tests/test_altimetry.py
@@ -3,47 +3,48 @@
 Tests of altimetry.
 """
 
+from numpy.testing import (assert_almost_equal)
 import pytest
 
-from pyfme.utils.altimetry import (geometric,
-                                   geopotential)
+from pyfme.utils.altimetry import (geopotential2geometric,
+                                   geometric2geopotential)
 
 
-def test1_geometric():
+def test1_geopotential2geometric():
 
     h = 11000
 
-    z = geometric(h)
+    z = geopotential2geometric(h)
     expected_z = 11019.025157232705
 
-    assert z == expected_z
+    assert_almost_equal(z, expected_z)
 
 
-def test2_geometric():
+def test2_geopotential2geometric():
 
     h = 20000
 
-    z = geometric(h)
+    z = geopotential2geometric(h)
     expected_z = 20062.982207526373
 
-    assert z == expected_z
+    assert_almost_equal(z, expected_z)
 
 
-def test1_geopotential():
+def test1_geometric2geopotential():
 
     z = 0.0
 
-    h = geopotential(z)
+    h = geometric2geopotential(z)
     expected_h = 0.0
 
-    assert h == expected_h
+    assert_almost_equal(h, expected_h)
 
 
-def test2_geopotential():
+def test2_geometric2geopotential():
 
     z = 86000
 
-    h = geopotential(z)
+    h = geometric2geopotential(z)
     expected_h = 84854.57642868205
 
-    assert h == expected_h
+    assert_almost_equal(h, expected_h)
