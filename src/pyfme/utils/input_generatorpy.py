839975ea14b14ea9d758c201693d1bd4c32c0aab
Oscar <olrosales@gmail.com>
2016-02-17 00:56:59 +0100

    Added changes in input_generator

--- src/pyfme/utils/input_generatorpy.py
+++ src/pyfme/utils/input_generatorpy.py
@@ -4,15 +4,14 @@ Python Flight Mechanics Engine (PyFME).
 Copyright (c) AeroPython Development Team.
 Distributed under the terms of the MIT License.
 
-<Inputs generator>
+Inputs generator
 -------------
-<Provides some typical inputs>
-< ... >
+Provides some typical inputs
 """
 import numpy as np
 
 
-def step(t_init, T, A, time, offset=0, var=0):
+def step(t_init, T, A, time, offset=0, var=None):
 
     """ step input
 
@@ -24,7 +23,7 @@ def step(t_init, T, A, time, offset=0, var=0):
     T : float
         time while input is running (s)
     A : float
-        peak to peak amplitude (rad) ### Should I choose other units?
+        peak to peak amplitude
     t : array_like
         time simulation vector (s)
     offset : float
@@ -35,15 +34,25 @@ def step(t_init, T, A, time, offset=0, var=0):
     -------
     step_input : array_like
     """
+    if var is None:
 
-    step_input = np.zeros_like(time)
-    step_input[(time >= t_init) & (time <= t_init + T)] = A + offset
-    step_input = step_input + var
+        step_input = np.ones_like(time) * offset
+        step_input[(time >= t_init) & (time <= t_init + T)] = A
+
+    else:
+
+        if np.size(var) == np.size(time):
+            step_input = offset + var
+            step_input[(time >= t_init) & (time <= t_init + T)] = A
+
+        else:
+
+            raise ValueError('var and time must have the same size')
 
     return (step_input)
 
 
-def doublet(t_init, T, A, time, offset=0, var=0):
+def doublet(t_init, T, A, time, offset=0, var=None):
 
     """ doublet input
 
@@ -55,7 +64,7 @@ def doublet(t_init, T, A, time, offset=0, var=0):
     T : float
         time while input is running (s)
     A : float
-        peak to peak amplitude (rad) ### Should I choose other units?
+        peak to peak amplitude
     t : array_like
         time simulation vector (s)
     offset : float
@@ -66,48 +75,29 @@ def doublet(t_init, T, A, time, offset=0, var=0):
     -------
     doublet_input : array_like
     """
+    if var is None:
 
-    doublet_input = np.zeros_like(time)
-    doublet_input[(time >= t_init) & (time < t_init + T/2)] = A/2 + offset
-    doublet_input[(time > t_init + T/2) & (time <= t_init + T)] = - A/2 +\
-        offset
-    doublet_input = doublet_input + var
+        doublet_input = np.ones_like(time)*offset
+        doublet_input[(time >= t_init) & (time <= t_init + T/2)] = A/2
+        doublet_input[(time > t_init + T/2) & (time <= t_init + T)] = - A/2
+
+    else:
+
+        if np.size(var) == np.size(time):
+            doublet_input = offset + var
+            doublet_input[(time >= t_init) & (time <= t_init + T/2)] = A/2
+            doublet_input[(time > t_init + T/2) & (time <= t_init + T)] = - A/2
+
+        else:
+
+            raise ValueError('var and time must have the same size')
 
     return (doublet_input)
 
 
-def impulse(t_init, A, time, offset=0, var=0):
+def sinusoide(t_init, T, A, time, phase=0, offset=0, var=None):
 
-    """ impulse input
-
-    Parameters
-    ----------
-
-    t_init : float
-             time initial to start (s)
-    A : float
-        peak to peak amplitude (rad) ### Should I choose other units?
-    t : array_like
-        time simulation vector (s)
-    offset : float
-    var : array_like
-          vector which contains previous perturbations
-
-    Returns
-    -------
-    impulse_input : array_like
-    """
-
-    impulse_input = np.zeros_like(time)
-    impulse_input[(time == t_init)] = A + offset
-    impulse_input = impulse_input + var
-
-    return (impulse_input)
-
-
-def harmonic(t_init, T, A, time, offset=0, var=0):
-
-    """ harmonic input
+    """ sinusoide input
 
     Parameters
     ----------
@@ -117,28 +107,44 @@ def harmonic(t_init, T, A, time, offset=0, var=0):
     T : float
         time while input is running (s)
     A : float
-        peak to peak amplitude (rad) ### Should I choose other units?
+        peak to peak amplitude (rad)
     t : array_like
         time simulation vector (s)
+    phase : float
+        sinusoidal phase (rad)
     offset : float
     var : array_like
           vector which contains previous perturbations
 
     Returns
     -------
-    harmonic_input : array_like
+    sinusoide_input : array_like
     """
 
-    harmonic_input = np.zeros_like(time)
-    harmonic_input[(time >= t_init) & (time <= t_init + T)] = A/2 * np.sin(2 *
-    np.pi * (time[(time >= t_init) & (time <= t_init + T)] - t_init) / T) +\
-        offset
-    harmonic_input = harmonic_input + var
+    time_input = time[(time >= t_init) & (time <= t_init + T)]
 
-    return (harmonic_input)
+    if var is None:
+
+        sinusoide_input = np.ones_like(time) * offset
+        sinusoide_input[(time >= t_init) & (time <= t_init + T)] = A/2 *\
+        np.sin(2 * np.pi * (time_input - t_init) / T + phase)
+
+    else:
+
+        if np.size(var) == np.size(time):
+
+            sinusoide_input = var + offset
+            sinusoide_input[(time >= t_init) & (time <= t_init + T)] = A/2 *\
+            np.sin(2 * np.pi * (time_input - t_init) / T + phase)
+
+        else:
+
+            raise ValueError('var and time must have the same size')
+
+    return (sinusoide_input)
 
 
-def ramp(t_init, T, A, time, offset=0, var=0):
+def ramp(t_init, T, A, time, offset=0, var=None):
 
     """ ramp input
 
@@ -150,7 +156,7 @@ def ramp(t_init, T, A, time, offset=0, var=0):
     T : float
         time while input is running (s)
     A : float
-        peak to peak amplitude (rad) ### Should I choose other units?
+        peak to peak amplitude (rad)
     t : array_like
         time simulation vector (s)
     offset : float
@@ -162,9 +168,73 @@ def ramp(t_init, T, A, time, offset=0, var=0):
     ramp_input : array_like
     """
 
-    ramp_input = np.zeros_like(time)
-    ramp_input[(time >= t_init) & (time <= t_init + T)] = (A / T) * (time[
-    (time >= t_init) & (time <= t_init + T)] - t_init) + offset
-    ramp_input = ramp_input + var
+    time_input = time[(time >= t_init) & (time <= t_init + T)]
+
+    if var is None:
+
+        ramp_input = np.ones_like(time) * offset
+        ramp_input[(time >= t_init) & (time <= t_init + T)] = (A / T) *\
+        (time_input - t_init)
+
+    else:
+
+        if np.size(var) == np.size(time):
+
+            ramp_input = var + offset
+            ramp_input[(time >= t_init) & (time <= t_init + T)] = (A / T) *\
+            (time_input - t_init)
+
+        else:
+
+            raise ValueError('var and time must have the same size')
 
     return (ramp_input)
+
+
+def harmonic(t_init, T, A, time, f, phase=0, offset=0, var=None):
+
+    """ sinusoide input
+
+    Parameters
+    ----------
+
+    t_init : float
+             time initial to start (s)
+    T : float
+        time while input is running (s)
+    A : float
+        peak to peak amplitude (rad)
+    t : array_like
+        time simulation vector (s)
+    f : float
+        sinusoidal frequency (s)
+    phase : float
+        sinusoidal phase (rad)
+    offset : float
+    var : array_like
+          vector which contains previous perturbations
+
+    Returns
+    -------
+    harmonic_input : array_like
+    """
+    time_input = time[(time >= t_init) & (time <= t_init + T)]
+
+    if var is None:
+
+        harmonic_input = np.ones_like(time) * offset
+        harmonic_input[(time >= t_init) & (time <= t_init + T)] = A/2 *\
+        np.sin(2 * np.pi * f * (time_input - t_init) + phase)
+
+    else:
+
+        if np.size(var) == np.size(time):
+
+            harmonic_input = var + offset
+            harmonic_input[(time >= t_init) & (time <= t_init + T)] = A/2 *\
+            np.sin(2 * np.pi * f * (time_input - t_init) + phase)
+        else:
+
+            raise ValueError('var and time must have the same size')
+
+    return (harmonic_input)
