a8d28ce0a937d19812a9cccf16f900e01c1d6109
Álex Sáez <alex.saez.12@gmail.com>
2016-06-15 00:08:43 +0200

    module with constants for pyfme

---
+++ src/pyfme/models/constants.py
@@ -0,0 +1,14 @@
+"""
+Python Flight Mechanics Engine (PyFME).
+Copyright (c) AeroPython Development Team.
+Distributed under the terms of the MIT License.
+
+Constant variables
+------------------
+
+"""
+
+GAMMA_AIR = 1.401  # Adiabatic index or ratio of specific heats (dry air at
+                   # 20º C)
+R_AIR = 287.05287  # J/(Kg·K)
+GRAVITY = 9.80665  # m/s^2
