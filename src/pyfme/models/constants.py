5da56daf43c72dd407b0fe5636c30036ebadafbc
Álex Sáez <alex.saez.12@gmail.com>
2016-06-18 17:44:36 +0200

    Added gravity model

--- src/pyfme/models/constants.py
+++ src/pyfme/models/constants.py
@@ -7,8 +7,15 @@ Constant variables
 ------------------
 
 """
-
+# AIR
 GAMMA_AIR = 1.401  # Adiabatic index or ratio of specific heats (dry air at
                    # 20º C)
 R_AIR = 287.05287  # J/(Kg·K)
+
+# GRAVITY of EARTH
 GRAVITY = 9.80665  # m/s^2
+# Standard Gravitational Parameter is the product of the gravitational
+# constant G and the mass M of the body.
+STD_GRAVITATIONAL_PARAMETER = 3.986004418e14  # m³/s²
+EARTH_MASS = 5.9722e24  # kg
+GRAVITATIONAL_CONSTANT = 6.67384e11 # N·m²/kg²
