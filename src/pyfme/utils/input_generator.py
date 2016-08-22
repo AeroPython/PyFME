3c464a643ad66e370290f633df8b18c77ec465bf
Álex Sáez <alex.saez.12@gmail.com>
2016-08-22 17:56:19 +0200

    sinusoid is calculated using harmonic

--- src/pyfme/utils/input_generator.py
+++ src/pyfme/utils/input_generator.py
@@ -88,47 +88,6 @@ def doublet(t_init, T, A, time, offset=0, var=None):
     return doublet_input
 
 
-def sinusoid(t_init, T, A, time, phase=0, offset=0, var=None):
-    """ Sinusoid input.
-
-    Parameters
-    ----------
-    t_init : float
-        Initial time (s).
-    T : float
-        Input signal length (s).
-    A : float
-        Peak to peak amplitude.
-    phase : float
-        sinusoidal phase (rad).
-    time : array_like
-        Time simulation vector (s).
-    offset : float
-        Signal offset.
-    var : array_like, opt
-        Array containing previous perturbations. Signal will be added to
-        this one.
-
-    Returns
-    -------
-    sinusoide_input : array_like
-    """
-
-    if var is None:
-        sinusoide_input = np.ones_like(time) * float(offset)
-    else:
-        if np.size(var) == np.size(time):
-            sinusoide_input = var + float(offset)
-        else:
-            raise ValueError('var and time must have the same size')
-
-    condition = (time >= t_init) & (time <= t_init + T)
-    time_input = time[condition]
-    sinusoide_input[condition] += \
-        A/2 * np.sin(2 * np.pi * (time_input - t_init) / T + phase)
-    return sinusoide_input
-
-
 def ramp(t_init, T, A, time, offset=0, var=None):
     """ Ramp input
 
@@ -206,3 +165,33 @@ def harmonic(t_init, T, A, time, f, phase=0, offset=0, var=None):
     harmonic_input[(time >= t_init) & (time <= t_init + T)] += \
         A / 2 * np.sin(2 * np.pi * f * (time_input - t_init) + phase)
     return harmonic_input
+
+
+def sinusoid(t_init, T, A, time, phase=0, offset=0, var=None):
+    """ Sinusoid input.
+
+    Parameters
+    ----------
+    t_init : float
+        Initial time (s).
+    T : float
+        Input signal length (s).
+    A : float
+        Peak to peak amplitude.
+    phase : float
+        sinusoidal phase (rad).
+    time : array_like
+        Time simulation vector (s).
+    offset : float
+        Signal offset.
+    var : array_like, opt
+        Array containing previous perturbations. Signal will be added to
+        this one.
+
+    Returns
+    -------
+    sinusoide_input : array_like
+    """
+    output = harmonic(t_init, T, A, time, f=1/T, phase=phase, offset=offset,
+                      var=var)
+    return output
