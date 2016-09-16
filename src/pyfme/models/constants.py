afae9bafb1e0c9b05a5ca027d7165795994a130c
Miquel Larsson <miquellarsson@gmail.com>
2016-09-16 10:36:59 +0200

    Cleaned, ordered and added comments to constants.py file

--- src/pyfme/models/constants.py
+++ src/pyfme/models/constants.py
@@ -8,26 +8,32 @@ Constant variables
 ------------------
 
 """
-# AIR
-GAMMA_AIR = 1.4    # Adiabatic index or ratio of specific heats (dry air at
-                   # 20º C)
-R_AIR = 287.05287  # J/(Kg·K)
-# Sea level conditions
-RHO_0 = 1.225  # density at sea level (kg/m3)
-P_0 = 101325  # pressure at sea level (Pa)
-SOUND_VEL_0 = 340.293990543  # sound speed at sea level (m/s)
 
-# GRAVITY of EARTH
-GRAVITY = 9.80665  # m/s^2
-# Standard Gravitational Parameter is the product of the gravitational
-# constant G and the mass M of the body.
-STD_GRAVITATIONAL_PARAMETER = 3.986004418e14  # m³/s²
-EARTH_MASS = 5.9722e24  # kg
-GRAVITATIONAL_CONSTANT = 6.67384e11 # N·m²/kg²
-EARTH_MEAN_RADIUS = 6371000 # m
+# AIR CONSTANTS
 
-# Conversions
-lbs2kg = 0.453592
-ft2m = 0.3048
-slug2kg = 14.5939
-slugft2_2_kgm2 = 1.35581795
+GAMMA_AIR = 1.4   								# Adiabatic index or ratio of specific heats (dry air at 20º C)
+R_AIR = 287.05287  								# Specific gas constant for dry air - J/(Kg·K)
+
+# Air at sea level conditions h=0 (m)
+
+RHO_0 = 1.225 					 				# Density at sea level (kg/m3)
+P_0 = 101325  							       	# Pressure at sea level (Pa)
+SOUND_VEL_0 = 340.293990543  					# Sound speed at sea level (m/s)
+
+
+# EARTH CONSTANTS
+
+GRAVITY = 9.80665  								# Gravity of Ethe Earth - m/s^2
+STD_GRAVITATIONAL_PARAMETER = 3.986004418e14    # Standard Gravitational Parameter is the product of the gravitational
+												# constant G and the mass M of the body - m³/s²
+EARTH_MASS = 5.9722e24  						# Mass of the Earth - kg
+GRAVITATIONAL_CONSTANT = 6.67384e11 			# Gravitational constant - N·m²/kg²
+EARTH_MEAN_RADIUS = 6371000 					# Mean radius of the Earth - m
+
+
+# CONVERSIONS
+
+lbs2kg = 0.453592								# Pounds (lb) to kilograms (kg)
+ft2m = 0.3048									# Feet (ft) to meters (m)
+slug2kg = 14.5939								# Slug to kilograms (kg)
+slugft2_2_kgm2 = 1.35581795						# Slug*feet^2 to kilograms*meters^2 (kg*m^2)
