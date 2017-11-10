c7d600ce496c7154e992d522eb79de5283638859
Álex Sáez <alex.saez.12@gmail.com>
2017-11-11 00:07:55 +0100

    added Envrionment to environment __init__

---
+++ src/pyfme/environment/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Python Flight Mechanics Engine (PyFME).
+Copyright (c) AeroPython Development Team.
+Distributed under the terms of the MIT License.
+
+Envrionment
+-----------
+
+"""
+from .environment import Environment
