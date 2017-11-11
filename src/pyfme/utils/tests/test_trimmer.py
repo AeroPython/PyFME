b91c3113c7b836bc214aac2cde08c21cf3811148
Álex Sáez <alex.saez.12@gmail.com>
2017-11-11 17:39:46 +0100

    added test checking steady state

--- src/pyfme/utils/tests/test_trimmer.py
+++ src/pyfme/utils/tests/test_trimmer.py
@@ -10,7 +10,9 @@ These values are hardcoded from the function results with the current costants.
 
 --------------------------
 """
+from itertools import product
 
+import pytest
 from numpy.testing import assert_almost_equal
 
 from pyfme.utils.trimmer import (rate_of_climb_cons, turn_coord_cons,
@@ -88,3 +90,60 @@ def test_turn_coord_cons_big_gamma():
     phi = turn_coord_cons(turn_rate, alpha, beta, TAS, gamma)
 
     assert_almost_equal(phi, expected_phi)
+
+
+trim_test_data = product(
+    [30, 45],
+    [500, 1000, 2000],
+    [0, 0.005, -0.005],
+    [0, 0.01, -0.01, 0.05, -0.05]
+)
+
+@pytest.mark.parametrize('TAS, h0, turn_rate, gamma', trim_test_data)
+def test_stationary_condition_trimming_Cessna172_ISA1972_NoWind_VerticalConstant(
+        TAS, h0, turn_rate, gamma
+):
+    import numpy as np
+
+    from pyfme.aircrafts import Cessna172
+    from pyfme.environment.environment import Environment
+    from pyfme.environment.atmosphere import ISA1976
+    from pyfme.environment.gravity import VerticalConstant
+    from pyfme.environment.wind import NoWind
+    from pyfme.models.state import EarthPosition
+    from pyfme.utils.trimmer import steady_state_trim
+
+    aircraft = Cessna172()
+    atmosphere = ISA1976()
+    gravity = VerticalConstant()
+    wind = NoWind()
+    environment = Environment(atmosphere, gravity, wind)
+
+    # Initial conditions.
+    psi0 = 1.0  # rad
+    x0, y0 = 0, 0  # m
+
+    pos0 = EarthPosition(x0, y0, h0)
+
+    controls0 = {'delta_elevator': 0.05,
+                 'delta_aileron': 0.2 * np.sign(turn_rate),
+                 'delta_rudder': 0.2 * np.sign(turn_rate),
+                 'delta_t': 0.5,
+                 'hor_tail_incidence': 0.0}
+
+    trimmed_state, trimmed_controls = steady_state_trim(
+        aircraft, environment, pos0, psi0, TAS, controls0, gamma, turn_rate,
+    )
+
+    # Acceleration
+    np.testing.assert_almost_equal(
+        trimmed_state.acceleration._value,
+        np.zeros_like(trimmed_state.acceleration._value),
+        decimal=2
+    )
+    # Angular acceleration
+    np.testing.assert_almost_equal(
+        trimmed_state.angular_accel._value,
+        np.zeros_like(trimmed_state.angular_accel._value),
+        decimal=2
+    )
